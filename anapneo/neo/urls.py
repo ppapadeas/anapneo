0c9cb37b3e778ee2fa57dcd2f66a80cea3ae1dd6
Pierros Papadeas <ppapadeas@gmail.com>
2013-04-20 13:28:50 +0300

    Initial templates and css fixes

--- anapneo/neo/urls.py
+++ anapneo/neo/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('anapneo.neo.views',
-    (r'^$', 'index'),
+    (r'^$', TemplateView.as_view(template_name="index.html")),
 )
--- anapneo/urls.py
+++ anapneo/urls.py
@@ -1,10 +1,20 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', include('anapneo.neo.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+## In DEBUG mode, serve media files through Django.
+if settings.DEBUG:
+    # Remove leading and trailing slashes so the regex matches.
+    media_url = settings.MEDIA_URL.lstrip('/').rstrip('/')
+    urlpatterns += patterns('',
+        url(r'^%s/(?P<path>.*)$' % media_url, 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT}),
+    )
