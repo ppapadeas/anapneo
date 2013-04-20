d5e235bb6e8bd77106b33f19e34d368a630f3a47
John Giannelos <johngiannelos@gmail.com>
2013-04-20 15:17:18 +0300

    Implement initial URL routing for neo app

--- anapneo/neo/views.py
+++ anapneo/neo/views.py
@@ -4,6 +4,17 @@ from django.shortcuts import render, get_object_or_404, get_list_or_404, redirec
 def index(request):
     return render(request, 'index.html', locals())
 
-
 def dashboard(request):
     return render(request, 'dashboard.html', locals())
+
+def show_neo(request, u_id):
+    pass
+
+def edit_or_create_neo(request, u_id):
+    pass
+
+def view_profile(request, slug):
+    pass
+
+def edit_profile(request, slug):
+    pass
