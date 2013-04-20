ca71edd59fa32a4a9e4d6897d950bd02e09a6e35
achronop <achronop@gmail.com>
2013-04-20 20:08:19 +0300

    Create Neo models and admin site

--- anapneo/neo/admin.py
+++ anapneo/neo/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from anapneo.neo.models import UserProfile
+from anapneo.neo.models import Neo
 
-admin.site.register(UserProfile)
+class NeoAdmin(admin.ModelAdmin):
+    list_display = ['user', 'observation_date', 'position_ra', 'position_dec', 
+                    'magnitude', 'exposure', 'instrument', 'aperture', 'telescope']
+    
+admin.site.register(Neo, NeoAdmin)
