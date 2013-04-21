aab364c9621d67347351e592f15449760c93340d
Nikos Roussos <nikos@roussos.cc>
2013-04-21 17:07:09 +0300

    first take on dashboard

--- anapneo/neo/forms.py
+++ anapneo/neo/forms.py
@@ -1,7 +1,14 @@
 from django.forms import ModelForm
-from anapneo.neo.models import Neo
+from anapneo.neo.models import Neo, UserProfile
+
 
 class NeoForm(ModelForm):
     class Meta:
         model = Neo
-    
+        exclude = ('user',)
+
+
+class UserProfileForm(ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user', 'email')
